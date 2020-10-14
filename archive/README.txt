3509bc3b135fec77824f96efafdb22a6e15fb216
Ed Vidal Jr <devel@mypi3-20.attlocal.net>
2020-10-14 07:23:36 -0600

    Steve Hoover provided the files in archive folder extra_files has files needed for Verilator.

---
+++ archive/README.txt
@@ -0,0 +1,2 @@
+This directory contains library content for IDE.
+It is currently hardcoded to exist in (or to be linked from) "data/lib/1" (under "/var/www/").
---
+++ archive/makerchip.sv
@@ -0,0 +1,29 @@
+`include "sp_default.vh" //_\SV
+
+// Provides Makerchip context in which user's module is invoked.
+// Instantiates design as code(.*) so user's module need only define the desired signals from
+// those available, which are:
+//   clk,
+//   reset
+//   cyc_cnt
+// sim_main.cpp drives clk and reset_async.
+// Simulation is ended on max cycles argument below, or assertion of passed/failed signal.
+// Additional testbench functionality can be added here, or within design using TLV.
+
+module makerchip(input logic clk, input logic reset_async, output logic passed, output logic failed);
+
+logic reset;
+//logic clk, reset_async;      // Generated in this module for DUT.
+//logic passed, failed;  // Returned from DUT to this module.  Passed must assert before
+                       //   max cycles, without failed having asserted.  Failed can be undriven.
+logic [31:0] cyc_cnt;
+
+always_ff @(posedge clk) begin
+   reset <= reset_async;
+   cyc_cnt <= reset ? 32'b1 : cyc_cnt + 32'b1;
+end
+
+// Instantiate main module.
+top top(.*);
+
+endmodule
